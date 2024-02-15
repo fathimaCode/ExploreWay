12c95e1c4aebb491fac579c70ff2a2b03836e250
fathimaCode <fathima.offical.msg@gmail.com>
2024-02-15 03:54:40 -0800

    some screens completed

--- app/src/main/java/com/exploreway/SplashScreenActivity.java
+++ app/src/main/java/com/exploreway/SplashScreenActivity.java
@@ -2,7 +2,14 @@ package com.exploreway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.exploreway.utils.SessionManager;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
@@ -10,5 +17,29 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Spinner dropdownBtn = findViewById(R.id.dropdownBtn);
+        String[] items = new String[]{"Choose your location","Coquitlam", "Burnaby"}; // Dropdown items
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dropdownBtn.setAdapter(adapter);
+
+
+        dropdownBtn.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = (String)parent.getItemAtPosition(position);
+                if(!selectedItem.equals("Choose your location")){
+                    SessionManager.getInstance(SplashScreenActivity.this).setSelectedLocation(selectedItem);
+                    Intent intent = new Intent(SplashScreenActivity.this,LoginActivity.class);
+
+                    startActivity(intent);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 }
